fed4925fb4ac30510277531e522a155ee5970085
Agemus <sopmort56@gmail.com>
2024-12-03 08:11:23 -0500

    laboratorio entregable 04

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log("estoy en js")
+
+
+
+let $resultado = document.getElementById("resul")
+
+function fsumar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    $resultado.innerHTML = "la suma es:"+ (num1+num2)
+}
+function frestar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    $resultado.innerHTML = "la resta es:" +(num1-num2)
+}
+function fmultiplicar(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    $resultado.innerHTML = "la multiplicacion es:" +(num1*num2)
+}
+function fdividir(){
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    if(num2===0)
+    {$resultado.innerHTML = "la division entre (0) cero no es posible"}
+    else{ $resultado.innerHTML = "la division es:" +(num1/num2)}
+}
